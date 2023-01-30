cd01a66b6c037006ecc2fa4d5aa78900d9d4bb63
MISMASJETU <fikejzv@gmail.com>
2023-01-30 08:07:10 +0100

    Color guesser - Hardmode

--- advanced/js/colorguesser.js
+++ advanced/js/colorguesser.js
@@ -5,6 +5,7 @@ const cg_reset = document.querySelector('.cg_reset');
 const cg_time = document.querySelector('.cg_time');
 const cg_score = document.querySelector('.cg_score');
 const cg_highscore = document.querySelector('.cg_highscore');
+const cg_hardmode = document.querySelector('.cg_hardmode');
 const number = 3;
 let time = [];
 let cg_answer = 0;
@@ -44,19 +45,38 @@ function constructButton(){
         tmpButton.style.width = '5em';
         tmpButton.style.height = '5em';
         if(i == cg_answer){
-            tmpButton.style.backgroundColor = cg_color;
+            tmpButton.style.backgroundColor = "#"+  cg_color;
             tmp_child.addEventListener('click', clickAnswer);
         } else {
-            let tmpColor = "#"+  Math.floor(Math.random()*16777215).toString(16);
-            tmpButton.style.backgroundColor = tmpColor;
+            let tmpColor = Math.floor(Math.random()*16777215).toString(16);
+            while(tmpColor.length < 6){
+                tmpColor += "0";
+            }
+
+            if(cg_hardmode.checked === true){
+                console.log(cg_color);
+                console.log(tmpColor);
+                tmpColor = colorBlender(tmpColor, cg_color, 0.6);
+                console.log(tmpColor);
+            }
+            tmpButton.style.backgroundColor = "#"+  tmpColor;
             tmp_child.addEventListener('click', clickWrong);
         }
 
         tmp_child.appendChild(tmpButton);
         cg_buttons.appendChild(tmp_child);
     }
-    cg_guess.style.backgroundColor = cg_color;
+    cg_guess.style.backgroundColor = "#"+  cg_color;
 }
+
+function colorBlender(colorA, colorB, amount) { //https://stackoverflow.com/questions/6367010/average-2-hex-colors-together-in-javascript
+    const [rA, gA, bA] = colorA.match(/\w\w/g).map((c) => parseInt(c, 16));
+    const [rB, gB, bB] = colorB.match(/\w\w/g).map((c) => parseInt(c, 16));
+    const r = Math.round(rA + (rB - rA) * amount).toString(16).padStart(2, '0');
+    const g = Math.round(gA + (gB - gA) * amount).toString(16).padStart(2, '0');
+    const b = Math.round(bA + (bB - bA) * amount).toString(16).padStart(2, '0');
+    return r + g + b;
+  }
 
 function clickAnswer(){
     //console.log(startTime);
@@ -97,8 +117,12 @@ function saveScore(){
 
 function playRound(){
     startTime = performance.now();
-    let color = "#"+  Math.floor(Math.random()*16777215).toString(16);
-    cg_guess.textContent = color;
+    let color = Math.floor(Math.random()*16777215).toString(16);
+    console.log(color.length);
+    while(color.length < 6){
+        color += "0";
+    }
+    cg_guess.textContent = "#"+  color;
     cg_color = color;
     cg_answer = Math.floor(Math.random() * cg_difficulty.value);
     //console.log(cg_answer);
