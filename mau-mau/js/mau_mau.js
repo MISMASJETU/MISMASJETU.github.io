89b0d5246be69888300eb26a8185390252141851
MISMASJETU <fikejzv@gmail.com>
2023-05-10 14:25:01 +0200

    continuing

--- mau-mau/js/mau_mau.js
+++ mau-mau/js/mau_mau.js
@@ -25,20 +25,26 @@ function bootScreen(){
     ctx.globalAlpha = 1;
     ctx.fillStyle = "#222222";
     ctx.fillRect(0, 0, sizeWidth, sizeHeight);
-
-
 }
 
 const getCursorPosition = (canvas, event) => { //https://blog.devgenius.io/how-to-get-the-coordinates-of-a-mouse-click-on-a-canvas-element-d5dc288c19e8
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    console.log(x + " " + y);
+    //console.log(x + " " + y);
 }
 
 canvas.addEventListener('mousedown', (e) => {
     getCursorPosition(canvas, e);
 })
+
+canvas.addEventListener('mousemove', (e) => {
+    checkCursorPosition();
+})
+
+function checkCursorPosition(){
+    //Will be used for howering above the arrows
+}
 
 function card(textureID, tags) {
     this.textureID = textureID;
@@ -57,7 +63,7 @@ function card(textureID, tags) {
     }
 }
 
-const numberOfTextures = 34;
+const numberOfTextures = 38;
 let loadedTextures = 0;
 let txt = [];
 function loadTexture(id){
@@ -66,9 +72,12 @@ function loadTexture(id){
     img.onload = function() {
         txt[id] = img;
         loadedTextures++;
-        console.log(loadedTextures);
+        //console.log(id);
         ctx.imageSmoothingEnabled = false;
         ctx.drawImage(txt[id], 10, 10,txt[id].width * 4, txt[id].height * 4);
+        if(loadedTextures == numberOfTextures){
+            startGame();
+        }
     };
 }
 
@@ -108,6 +117,10 @@ function loadTextures(){
     loadTexture("hq");
     loadTexture("j1");
     loadTexture("j2");
+    loadTexture("left");
+    loadTexture("left_hover");
+    loadTexture("right");
+    loadTexture("right_hover");
 }
 
 createCards();
@@ -146,4 +159,64 @@ function createCards(){
     playedCards.push(new card("hq",["h","q"]));
     playedCards.push(new card("j1",["h","s","c","d","1","7","8","9","10","j","k","q"]));
     playedCards.push(new card("j2",["h","s","c","d","1","7","8","9","10","j","k","q"]));
+}
+
+function randomizeDeck(){
+    unplayedCards.sort(() => Math.random() - 0.5);
+    /*for(let i = 0; i < unplayedCards.length; i++){
+        console.log(unplayedCards[i]);
+    }*/
+}
+
+function drawCard(side){
+    let tmp = unplayedCards[unplayedCards.length - 1];
+    unplayedCards.pop();
+    if(side == "player"){
+        playerCards.push(tmp);
+    }
+    else
+    {
+        aiCards.push(tmp);
+    }
+}
+
+//startGame();
+function startGame(){
+    unplayedCards = playedCards;
+    playedCards = [];
+    randomizeDeck();
+    let starterCards = 10;
+    for(let i = 0; i < starterCards; i++){
+        drawCard("player");
+    }
+    for(let i = 0; i < starterCards; i++){
+        drawCard("ai");
+    }
+    renderPlayerHand();
+}
+
+function renderPlayerHand(){
+    let size = Math.min(playerCards.length, 7);
+    let offset =  (size - 1) / 2;
+    const df = (sizeWidth / 2) - (offset * 120); //the point card renders from
+    for(let i = 0; i < size; i++){
+        renderCard(playerCards[i], df + (i * 120), sizeHeight - 100);
+    }
+    renderArrows();
+}
+
+function renderCard(card, x, y){
+    const img = card;
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(txt[card.textureID], x - txt[card.textureID].width * 2, y - txt[card.textureID].height * 2,txt[card.textureID].width * 4, txt[card.textureID].height * 4);
+}
+
+function renderArrows(){
+    renderTexture("left", 45, sizeHeight - 100);
+    renderTexture("right", sizeWidth - 45, sizeHeight - 100);
+}
+
+function renderTexture(id, x, y){
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(txt[id], x - txt[id].width * 2, y - txt[id].height * 2,txt[id].width * 4, txt[id].height * 4);
 }
